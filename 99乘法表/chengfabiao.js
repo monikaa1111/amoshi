73d07fc8e0da009512520c4cec1aec63a87bbb2c
monikaa1111 <1342991058@qq.com>
2020-04-21 21:50:09 +0800

    amoshi first commit

---
+++ 99乘法表/chengfabiao.js
@@ -0,0 +1,27 @@
+/*for(var i = 1;i<10;i++)
+{
+	for (var j = 1;j<10;j++)
+{
+	if(j<=i)
+	{
+		document.write(i+'*'+j+'='+i+'*'+j);
+	}
+	}
+}*/
+
+/*for (var i=1;i<10;i++){
+           for(var j=1;j<=i;j++){
+           document.write(j+"*"+i+"="+j*i);
+          }document.write("<br/>");
+    }*/
+
+
+for (var i=1;i<10;i++)
+{
+	for (var j=1;j<=i;j++)
+	{
+		
+			document.write(j+"*"+i+"="+j*i);
+		
+}
+}
---
+++ js/i.js
@@ -0,0 +1,60 @@
+//李四的个人信息
+alert("hellow");
+var name = "李四";
+var age = 19;
+var school = "十三中学";
+var user_id = 1780;
+var user_money = 900;
+var user_address = "陕西延安";
+var flag = "true";
+document.write("姓名："+name+"<br />");
+document.write("年龄："+age+"<br />");
+document.write("所在学校："+school+"<br />");
+document.write("学号："+user_id+"<br />");
+document.write("零花钱："+user_money+"<br />");
+document.write("家庭地址："+user_address+"<br />");
+
+
+document.write("姓名："+name+"年龄："+age+"所在学校："+school+"学号："+user_id+"零花钱："+user_money+"家庭地址："+user_address+flag);
+
+
+	var num = 9;
+			if(num%2==0)
+			{
+				document.write("偶数");
+			}
+			else
+			{
+				document.write("奇数");
+			}
+			
+			
+			
+		 for (var  i = 1; i <= 100 ; i++)
+		 {
+		 	if (i % 2 != 0 )
+		 	{
+	 			document.write(i+"<br>")
+	 		}
+	 	}
+		 sum = 0;
+		 for(var i = 1;i<=500;i++)
+		 {
+		 	sum = sum+i;
+		 }
+		 document.write(sum+i);
+//输出5个hello
+for(var i = 1;i<=5;i++)
+{
+	document.write("hello");
+}
+//输出1-100的3的余数
+for(var i = 1;i<=100;i++)
+{
+	if(i%3==0){
+		document.write(i+"<br>");
+	}
+}
+	 
+
+	
